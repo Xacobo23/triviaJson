0a1f1c4213f20c3affce56d0d78fd1e1140dac6c
A23XacoboBC <a23xacobobc@iessanclemente.net>
2024-10-18 10:22:12 +0200

    A

--- src/main/java/org/example/AppTrivial.java
+++ src/main/java/org/example/AppTrivial.java
@@ -3,6 +3,9 @@ package org.example;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class AppTrivial {
@@ -15,6 +18,31 @@ public class AppTrivial {
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        System.out.println(gson.toJson(pregunta));
+//        System.out.println(gson.toJson(pregunta));
+
+        File file = new File("./text.txt");
+
+        try(var bw = Files.newBufferedWriter(Paths.get("preguntaa.json"))){
+            gson.toJson(pregunta, bw);
+        }catch (IOException e){
+            System.out.println("AAA");
+        }
+        try(var br = Files.newBufferedReader(Paths.get("preguntaa.json"))){
+            String line;
+            while ((line = br.readLine()) != null){
+
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+//        try(var bw = Files.newBufferedWriter(Paths.get("pregunta.json"))){
+//            bw.write(gson.toJson(pregunta));
+//        }catch (IOException e){
+//            System.out.println("AAA");
+//        }
+//
+//        Files.writeString(Paths.get("a.txt"), gson.toJson(pregunta));
     }
 }
